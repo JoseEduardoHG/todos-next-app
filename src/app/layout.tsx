7673b59b1f4bae10d69108ab1b062890701aef29
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-02-08 09:32:55 -0500

    feat: add Header component to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import Header from '@/components/Header';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Todos Next App",
-  description: "A Todos app with authentication",
+  title: 'Todos Next App',
+  description: 'A Todos app with authentication',
 };
 
 export default function RootLayout({
@@ -15,7 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <Header title='todos' />
       <body className={inter.className}>{children}</body>
     </html>
   );
