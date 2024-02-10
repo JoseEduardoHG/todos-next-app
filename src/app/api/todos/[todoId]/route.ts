481ad79170f712a6c43082aa881da242bd41001c
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-02-09 19:32:20 -0500

    feat: add PUT /api/todos/:todoId route

--- src/app/api/todos/[todoId]/route.ts
+++ src/app/api/todos/[todoId]/route.ts
@@ -58,3 +58,40 @@ export async function DELETE(
     );
   }
 }
+
+export async function PUT(
+  req: NextRequest,
+  context: { params: { todoId: string } },
+) {
+  try {
+    await connectDB();
+    const { title, isCompleted } = (await req.json()) as {
+      title: string;
+      isCompleted: boolean;
+    };
+    const todoId = context.params.todoId;
+
+    const todo = await Todo.findByIdAndUpdate(todoId, { title, isCompleted });
+    if (!todo)
+      return NextResponse.json(
+        { status: 'error', message: 'Todo not found' },
+        { status: 404 },
+      );
+
+    return NextResponse.json(
+      { status: 'success', data: todo },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof MongooseError)
+      return NextResponse.json(
+        { status: 'error', message: error.message },
+        { status: 400 },
+      );
+
+    return NextResponse.json(
+      { status: 'error', message: 'Oops! Something went wrong!' },
+      { status: 500 },
+    );
+  }
+}
