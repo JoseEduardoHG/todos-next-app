a48864b23c8a93fc75aae2e0af8cad2031bf7021
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-02-09 19:21:33 -0500

    feat: add DELETE /api/todos/:todoId route

--- src/app/api/todos/[todoId]/route.ts
+++ src/app/api/todos/[todoId]/route.ts
@@ -30,3 +30,31 @@ export async function GET(
     );
   }
 }
+
+export async function DELETE(
+  _req: NextRequest,
+  context: { params: { todoId: string } },
+) {
+  try {
+    await connectDB();
+
+    const todoId = context.params.todoId;
+    const todo = await Todo.findByIdAndDelete(todoId);
+
+    return NextResponse.json(
+      { status: 'success', data: todo },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof MongooseError)
+      return NextResponse.json(
+        { status: 'error', message: error.message },
+        { status: 400 },
+      );
+
+    return NextResponse.json(
+      { status: 'error', message: 'Oops! Something went wrong!' },
+      { status: 500 },
+    );
+  }
+}
