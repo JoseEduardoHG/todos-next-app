1cc39872b78be72cd22565b8cd3f3e89ce928a4b
JoseEduardoHG <101235315+JoseEduardoHG@users.noreply.github.com>
2024-02-09 19:04:42 -0500

    feat: add POST /api/todos route

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,7 +1,7 @@
 import { connectDB } from '@/db/connectDB';
 import Todo from '@/models/Todo';
 import { MongooseError } from 'mongoose';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
   try {
@@ -10,6 +10,30 @@ export async function GET() {
 
     return NextResponse.json(
       { status: 'success', data: todos },
+      { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof MongooseError)
+      return NextResponse.json(
+        { status: 'error', message: error.message },
+        { status: 400 },
+      );
+
+    return NextResponse.json(
+      { status: 'error', message: 'Oops! Something went wrong!' },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+    const body = await req.json();
+    const todo = await Todo.create(body);
+
+    return NextResponse.json(
+      { status: 'success', data: todo },
       { status: 200 },
     );
   } catch (error) {
